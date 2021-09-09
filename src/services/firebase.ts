dd7fcc9fec2852fd98ab11e60fe0d1461cd54bfa
Marlon Andrade <marlon.aswa@gmail.com>
2021-09-09 00:11:57 -0300

    Aula 2 - resolve conflits

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,13 +1,9 @@
 import firebase from 'firebase/app';
-<<<<<<< HEAD
-=======
 
->>>>>>> 19b0af957158b450465ebe22d9ffafe16ff7c6ad
 import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -19,24 +15,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
- const auth = firebase.auth();
- const database = firebase.database();
-
-export { firebase, auth, database }
-=======
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
-  };
-
-firebase.initializeApp(firebaseConfig);
-
 const auth = firebase.auth();
 const database = firebase.database();
 
-export { firebase, auth, database };
->>>>>>> 19b0af957158b450465ebe22d9ffafe16ff7c6ad
+export { firebase, auth, database }
